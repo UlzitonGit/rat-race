bc7d9779220e62b60b3145d919f9ba105c64ea7e
UlzitonGit <anton.ulzibat@gmail.com>
2024-09-11 00:40:18 +0300

    character

--- Rat Race/Assets/Scripts/PlayerController.cs
+++ Rat Race/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,32 +9,59 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     EnemyDetecter enemyDetecter;
     bool concentrate = false;
+    [SerializeField] Animator animator;
     int enemyToConcentrate = 0;
+    Vector3 moveDireciton;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         enemyDetecter = GetComponentInChildren<EnemyDetecter>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+
+
+
+        Walk();
+        Concentration();
+        Animation();
+    }
+    private void Walk()
     {
         float _Xspeed = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float _Yspeed = Input.GetAxisRaw("Vertical") * moveSpeed;
+        moveDireciton = new Vector3(_Xspeed, 0, _Yspeed);
+        moveDireciton.Normalize();
         rb.velocity = new Vector3(_Xspeed, rb.velocity.y, _Yspeed);
-        if(rb.velocity.magnitude > 0.1f && concentrate == false)
+        if (rb.velocity.magnitude > 0.1f && concentrate == false)
         {
-            transform.rotation = Quaternion.LookRotation(new Vector3(_Xspeed,0 , _Yspeed));
+
+            Quaternion toRot = Quaternion.LookRotation(moveDireciton, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, 360 * Time.deltaTime);
         }
-
-
+    }
+    private void Animation()
+    {
+        if (rb.velocity.magnitude >= 0.1) animator.SetBool("walk", true);
+        if (rb.velocity.magnitude <= 0.1) animator.SetBool("walk", false);
+        animator.SetBool("Concentrate", concentrate);
+        animator.SetFloat("Input", rb.velocity.magnitude);
+        animator.SetFloat("Horizontal", moveDireciton.x * Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", moveDireciton.y * Input.GetAxis("Vertical"));
+    }
+    private void Concentration()
+    {
         if (enemyDetecter.enemies.Count != 0 && concentrate == true)
         {
             if (enemyToConcentrate > enemyDetecter.enemies.Count - 1) enemyToConcentrate = 0;
             transform.LookAt(enemyDetecter.enemies[enemyToConcentrate].transform);
         }
-        if(enemyDetecter.enemies.Count == 0 && concentrate == true)
+        if (enemyDetecter.enemies.Count == 0 && concentrate == true)
         {
             enemyToConcentrate = 0;
             concentrate = false;
@@ -51,9 +79,9 @@ public class PlayerController : MonoBehaviour
         {
 
             if (enemyToConcentrate >= enemyDetecter.enemies.Count - 1) enemyToConcentrate = 0;
-            else enemyToConcentrate ++;
+            else enemyToConcentrate++;
             print(enemyToConcentrate);
-           
+
         }
     }
 }
