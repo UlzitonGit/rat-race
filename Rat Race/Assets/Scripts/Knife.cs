82d47544bdbf4dec9ee1b1b2cd51163739509c06
gaudy228 <228822009900Art@gmail.com>
2024-09-12 23:51:57 +0300

    mana is spent when attacking

--- Rat Race/Assets/Scripts/Knife.cs
+++ Rat Race/Assets/Scripts/Knife.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Knife : WeaponParent
 {
-   
+    [SerializeField] private GameObject ManaUIScript;
+    private ManaUI manaUI;
+    public float HitMinusMana;
+    
     // Start is called before the first frame update
     void Start()
     {
-     
+      manaUI = ManaUIScript.GetComponent<ManaUI>();
     }
 
     // Update is called once per frame
@@ -17,6 +20,7 @@ public class Knife : WeaponParent
         if(Input.GetKey(KeyCode.Mouse0) && canAttack == true)
         {
             AttackMele();
+            manaUI.mana -= HitMinusMana;
         }
     }
 }
