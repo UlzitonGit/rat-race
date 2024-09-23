36a9da258ed386c653d17459d3088dadaf4354f7
UlzitonGit <anton.ulzibat@gmail.com>
2024-09-23 21:05:12 +0300

    Granade launcher anim

--- Rat Race/Assets/Scripts/Weapon/WeaponSwitch.cs
+++ Rat Race/Assets/Scripts/Weapon/WeaponSwitch.cs
@@ -6,6 +6,8 @@ public class WeaponSwitch : MonoBehaviour
 {
     [SerializeField] GameObject knifeModel;
     [SerializeField] GameObject bowModel;
+    [SerializeField] GameObject launcherModel;
+    [SerializeField] Animator animator;
     Knife knife;
     Bow bow;
     Launcher launcher;
@@ -19,27 +21,33 @@ public class WeaponSwitch : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Alpha1))
         {
+            animator.SetBool("Launcher", false);
             bow.Switch();
             knife.enabled = true;
             knifeModel.SetActive(true);
+            launcherModel.SetActive(false);
             bow.enabled = false;
             bowModel.SetActive(false);
             launcher.enabled = false;
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
+            animator.SetBool("Launcher", false);
             knife.enabled = false;
             knifeModel.SetActive(false);
+            launcherModel.SetActive(false);
             bow.enabled = true;
             bowModel.SetActive(true);
             launcher.enabled = false;
         }
         if (Input.GetKey(KeyCode.Alpha3))
         {
+            animator.SetBool("Launcher", true);
             launcher.enabled = true;
             bow.Switch();
             knife.enabled = false;
             knifeModel.SetActive(false);
+            launcherModel.SetActive(true);
             bow.enabled = false;
             bowModel.SetActive(false);
         }
